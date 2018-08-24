ba2284101bf79253d09cbef1e6d6224ea16908f4
tulenkovR <kubmaster@mail.ru>
2018-08-24 15:47:56 +0300

    Add controllers

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :todo do
-    title { Faker::TheITCrowd.quote }
+    title { Faker::Lorem.word }
     created_by { Faker::Date.between(2.days.ago, Date.today) }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,5 +5,4 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   it { should belong_to(:todo) }
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:done) }
 end
