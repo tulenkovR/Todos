db5ee6244cf35c54f7c6c8244e05ca5c36029ae1
tulenkovR <kubmaster@mail.ru>
2018-08-24 15:01:36 +0300

    Setup rspec and gems

--- config/application.rb
+++ config/application.rb
@@ -22,14 +22,18 @@ module Todos
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_spec: true
+      g.fixture_replacement :factory_bot
+      g.factory_bot dir: 'spec/factories'
+    end
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
